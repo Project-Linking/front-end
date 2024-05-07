6757ceb2940c45425cf92ebc1e72cb6d60558ff1
songhaechan <bukak2019@naver.com>
2024-05-07 17:32:46 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <template v-slot:prepend>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </template>
-      <v-app-bar-title>Application Bar</v-app-bar-title>
+      <v-app-bar-title>Project Linking</v-app-bar-title>
+      <v-btn variant="text" size="small"> Login </v-btn>
     </v-app-bar>
   </v-app>
 </template>
@@ -12,7 +13,6 @@
 <script>
 export default {
   name: "App",
-  components: {},
 };
 </script>
 
