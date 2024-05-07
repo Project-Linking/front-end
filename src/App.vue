8f73b4f2a021872d9342a2364004ac5ac3e825f4
songhaechan <bukak2019@naver.com>
2024-05-07 17:51:08 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </template>
-      <v-app-bar-title>Project Linking</v-app-bar-title>
-      <v-btn variant="text" size="small"> Login </v-btn>
-    </v-app-bar>
+    <navigation-bar></navigation-bar>
   </v-app>
 </template>
 
 <script>
+import NavigationBar from "./components/NavigationBar.vue";
 export default {
+  components: { NavigationBar },
   name: "App",
 };
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Project Linking</v-app-bar-title>
+    <v-btn variant="text" size="small"> Login </v-btn>
+  </v-app-bar>
+</template>
