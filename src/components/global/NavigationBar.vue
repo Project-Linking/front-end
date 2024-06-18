34613951611cfe88b128aadbda5bd9f88907ace6
SangWoon123 <alrax3574@naver.com>
2024-06-18 17:58:16 +0900

    fix: 네비게이션 바 수정

--- src/components/global/NavigationBar.vue
+++ src/components/global/NavigationBar.vue
@@ -1,42 +1,41 @@
 <template>
     <v-toolbar dense class="custom-toolbar">
-        <v-btn icon color="primary">
-            <v-icon>mdi-menu</v-icon>
-        </v-btn>
-        <v-btn icon color="primary" to="/home">
-            <v-icon>mdi-home-outline</v-icon>
-        </v-btn>
+        <v-row justify="space-between" align="center">
+            <v-col cols="auto">
+                <v-btn icon color="primary">
+                    <v-icon>mdi-menu</v-icon>
+                </v-btn>
+                <v-btn icon color="primary" to="/home">
+                    <v-icon>mdi-home-outline</v-icon>
+                </v-btn>
 
-        <v-divider class="mx-3 align-self-center" length="24" thickness="2" vertical />
-        <v-btn variant="text" to="/post"> 프로젝트 게시판 </v-btn>
-        <v-btn variant="text"> 프로젝트 게시판 </v-btn>
-        <v-btn variant="text"> 프로젝트 게시판 </v-btn>
-        <v-btn variant="text"> 프로젝트 게시판 </v-btn>
-        <v-btn variant="text"> 프로젝트 게시판 </v-btn>
-        <v-btn variant="text"> 프로젝트 게시판 </v-btn>
+                <v-divider class="mx-3" length="24" thickness="2" vertical />
 
-        <v-spacer></v-spacer>
-        <v-text-field
-            rounded
-            max-width="200"
-            variant="outlined"
-            prepend-inner-icon="mdi-magnify"
-            placeholder="키워드로 검색해보세요!"
-            hide-details
-        ></v-text-field>
-        <user-avatar v-if="isLoggedIn"></user-avatar>
-        <v-btn v-if="!isLoggedIn" to="/login" text small>Login</v-btn>
+                <v-btn variant="text" to="/post"> 프로젝트 게시판 </v-btn>
+                <v-btn variant="text" to="/post"> 지원서 </v-btn>
+            </v-col>
+            <v-col cols="auto">
+                <v-btn v-if="!isLoggedIn" to="/login">로그인</v-btn>
+                <v-btn v-if="!isLoggedIn" to="/signup">회원가입</v-btn>
+                <v-btn v-if="isLoggedIn" @click="logout">로그아웃</v-btn>
+            </v-col>
+        </v-row>
     </v-toolbar>
 </template>
 
 <script>
-import UserAvatar from '@/components/global/UserAvatar';
 import { mapState } from 'pinia';
 import { useAuthStore } from '@/store/useAuthStore';
 export default {
-    components: { UserAvatar },
+    components: {},
     computed: {
         ...mapState(useAuthStore, ['isLoggedIn']),
+    },
+    methods: {
+        logout() {
+            const authStore = useAuthStore();
+            authStore.deleteAuth();
+        },
     },
 };
 </script>
