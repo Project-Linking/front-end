63f349fe796ba9f8c7db374f9360abd278093286
SangWoon123 <alrax3574@naver.com>
2024-06-28 13:58:59 +0900

    feat: 게시글 작성 컴포넌트 버튼 추가

---
+++ src/components/post/QuickWriteDialog.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container class="write-dialog">
+        <v-speed-dial>
+            <template v-slot:activator="{ props: activatorProps }">
+                <v-fab v-bind="activatorProps" size="large" icon="mdi-pencil" @click="createPost"></v-fab>
+            </template>
+        </v-speed-dial>
+    </v-container>
+</template>
+
+<script>
+export default {
+    methods: {
+        createPost() {
+            this.$router.push('/post/create');
+        },
+    },
+};
+</script>
+
+<style scoped>
+.write-dialog {
+    position: fixed;
+    left: 95vh;
+    bottom: 50px;
+    z-index: 100;
+}
+</style>
