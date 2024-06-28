2c16e2e79b66d8f844f1f6565d340fa6287ddae9
SangWoon123 <alrax3574@naver.com>
2024-06-28 13:58:01 +0900

    fix: 게시글 수정

--- src/components/post/PostCard.vue
+++ src/components/post/PostCard.vue
@@ -69,15 +69,15 @@
             </v-card>
         </v-row>
         <!-- 팝업 -->
-        <v-overlay v-model="overlay" absolute class="d-flex align-center justify-center">
+        <v-overlay v-model="overlay" class="d-flex align-center justify-center">
             <v-card width="90%" height="90%" class="ma-auto">
                 <v-row width="100%" height="100%" class="d-flex flex-nowrap ma-auto">
                     <v-col cols="auto">
                         <v-card-title class="d-flex justify-start">Hello World</v-card-title>
                         <v-card-subtitle class="d-flex align-center justify-start mb-5">
                             <v-chip>유저닉네임</v-chip>
-                            <v-divider vertical class="mx-2"></v-divider>
-                            2024.06.24
+                            <v-divider vertical class="mx-2" color="info"></v-divider>
+                            <span>2024.06.24</span>
                         </v-card-subtitle>
                         <!-- 자세한 정보 -->
                         <PostInfoDetailVue />
@@ -87,17 +87,26 @@
                         <v-divider vertical color="info"> </v-divider>
                     </v-col>
                     <v-col cols="8">
-                        <v-card-text class="content-text text-body-2">
+                        <v-card-text class="text-body-2">
                             (간단 소개 문구) Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis
                             dicta esse molestias vero hic laudantium provident nisi eos quasi iusto alias sequi, aut
-                            aliquid voluptatibus commodi! Minima, eum voluptates?
+                            aliquid voluptatibus commodi! Minima, eum voluptates? (간단 소개 문구) Lorem ipsum dolor sit
+                            amet consectetur adipisicing elit. Officiis facilis dicta esse molestias vero hic laudantium
+                            provident nisi eos quasi iusto alias sequi, aut aliquid voluptatibus commodi! Minima, eum
+                            voluptates? (간단 소개 문구) Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Officiis facilis dicta esse molestias vero hic laudantium provident nisi eos quasi iusto
+                            alias sequi, aut aliquid voluptatibus commodi! Minima, eum voluptates? (간단 소개 문구)
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis dicta esse
+                            molestias vero hic laudantium provident nisi eos quasi iusto alias sequi, aut aliquid
+                            voluptatibus commodi! Minima, eum voluptates?
                         </v-card-text>
-                        <v-row class="ml-2">
+                        <v-row class="ml-2 mt-5">
+                            <!-- 이미지 -->
                             <v-col cols="auto">
                                 <v-img
                                     cover
-                                    :width="100"
-                                    :height="100"
+                                    :width="200"
+                                    :height="200"
                                     src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
                                 ></v-img>
                             </v-col>
@@ -112,7 +121,7 @@
                         </v-row>
                     </v-col>
                     <!-- 버튼 -->
-                    <v-col cols="1" class="d-flex justify-end align-start">
+                    <v-col cols="auto" class="d-flex justify-end align-start">
                         <v-btn flat icon @click="closeOverlay">
                             <v-icon>mdi-close</v-icon>
                         </v-btn>
