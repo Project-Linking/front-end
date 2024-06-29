31ba912368836d66307f8e000b837653feedc50a
SangWoon123 <alrax3574@naver.com>
2024-06-29 15:51:44 +0900

    fix: 로그아웃 수정

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,25 +1,21 @@
-import { defineStore } from "pinia";
-import { useStorage } from "@vueuse/core";
+import { defineStore } from 'pinia';
+import { useStorage } from '@vueuse/core';
+import router from '@/plugins/router';
 
-export const useAuthStore = defineStore("auth", {
-  state: () => ({
-    accessToken: "",
-    isLoggedIn: useStorage("isLoggedIn", false),
-    nickname: useStorage("nickname", ""),
-    imgUri: useStorage("imgUri", ""),
-  }),
-  actions: {
-    saveAuth(token, nickname, imgUri) {
-      (this.accessToken = token),
-        (this.isLoggedIn = true),
-        (this.nickname = nickname),
-        (this.imgUri = imgUri);
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        accessToken: '',
+        isLoggedIn: useStorage('isLoggedIn', false),
+        nickname: useStorage('nickname', ''),
+        imgUri: useStorage('imgUri', ''),
+    }),
+    actions: {
+        saveAuth(token, nickname, imgUri) {
+            (this.accessToken = token), (this.isLoggedIn = true), (this.nickname = nickname), (this.imgUri = imgUri);
+        },
+        deleteAuth() {
+            (this.accessToken = null), (this.isLoggedIn = false), (this.nickname = null), (this.imgUri = null);
+            router.push('/post');
+        },
     },
-    deleteAuth() {
-      (this.accessToken = null),
-        (this.isLoggedIn = false),
-        (this.nickname = null),
-        (this.imgUri = null);
-    },
-  },
 });
